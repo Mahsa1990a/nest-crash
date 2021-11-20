519753460a6f0c0dfb7dccc691eed976a1870345
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-20 18:13:07 -0500

    created users module, controller and service and Get request

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
-@Controller()
+//Controller is a Decorator
+@Controller('app') //'app': basic path, means visit http://localhost:3000/app 
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get()
+  //@Get is a Method Decorator
+  @Get('hello') //now visit: http://localhost:3000/app/hello
   getHello(): string {
     return this.appService.getHello();
   }
