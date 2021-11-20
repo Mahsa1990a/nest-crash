3e70a35245e94bc52fb97dc8f238c52ebb60ece2
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-20 18:36:13 -0500

    added methods in service and pass into controller

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,18 +1,23 @@
 import { Controller, Get, Param } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
 
+  constructor(private usersSrvice: UsersService) {}
+
   @Get()
   getUsers(): any {
-    return [{id: 0}]
+    // return [{id: 0}] //update after adding methods in service:
+    return this.usersSrvice.findAll();
   }
 
   @Get(':id') // : means it's a dynamic value
   //param decorator
   getUserById(@Param('id') id: string): any {
-    return {
-      id: Number(id)
-    }
+    // return {
+    //   id: Number(id)
+    // } //update after adding methods in service:
+    return this.usersSrvice.findById(Number(id));
   }
 }
--- src/users/users.service.ts
+++ src/users/users.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
 
 @Injectable()
-export class UsersService {}
+export class UsersService {
+  private users: any = [
+    {id: 0, name: 'Mahsa'}
+  ];
+
+  findAll() { //getUsers
+    return this.users;
+  }
+
+  findById(userId: number) { //getUserById
+    return this.users.find(user => user.id === userId);
+  }
+
+}
