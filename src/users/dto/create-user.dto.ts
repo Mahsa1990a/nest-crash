0258b1e39a6b9676684d64af2a3fb7e1320697a1
Mahsa Arabameri <amerimahsa@yahoo.com>
2021-11-20 20:44:27 -0500

    added schema for request by adding ApiProperty

--- src/users/dto/create-user.dto.ts
+++ src/users/dto/create-user.dto.ts
@@ -1,4 +1,11 @@
+import { ApiProperty } from "@nestjs/swagger";
+
 // dto: data tranfor object => schema representation of whatever object you wanna use as an inter media to transfer information
 export class CraeteUserDto {
+
+  @ApiProperty() //shows the schema
   name: string
+
+  @ApiProperty({required: false}) // ? & required: false => means age optional
+  age?: number
 } 
